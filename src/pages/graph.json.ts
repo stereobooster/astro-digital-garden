05c2ffa8da2bce391bac9e2a8861f469b2a80092
stereobooster <stereobooster@gmail.com>
2024-04-16 22:09:27 +0200

    small fix

--- src/pages/graph.json.ts
+++ src/pages/graph.json.ts
@@ -2,5 +2,6 @@ import { bdb } from "../lib/braindb.mjs";
 import { toGraphologyJson } from "../lib/graph";
 
 export async function GET() {
+  await bdb.ready();
   return new Response(JSON.stringify(await toGraphologyJson(bdb)));
 }
