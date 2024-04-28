3a8aab6daf7cf0c06f0e40c91180dd0cf609db6d
stereobooster <stereobooster@gmail.com>
2024-04-28 17:51:12 +0200

    experiments

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import rehypeExternalLinks from "rehype-external-links";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import remarkMermaidjs from "remark-mermaidjs";
+// import rehypeMermaid from "rehype-mermaid";
+// import { astroExpressiveCode, addClassName } from "astro-expressive-code";
+
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import Icons from "unplugin-icons/vite";
@@ -17,6 +20,42 @@ await bdb.ready();
 export default defineConfig({
   site: "https://astro-digital-garden.stereobooster.com",
   integrations: [
+    // astroExpressiveCode({
+    //   customConfigPreprocessors: {
+    //     preprocessAstroIntegrationConfig: () => {
+    //       // Add the `not-content` class to all rendered blocks to prevent them from being affected
+    //       // by Starlight's default content styles
+    //       const plugins = [
+    //         {
+    //           name: "Starlight Plugin",
+    //           hooks: {
+    //             postprocessRenderedBlock: ({ renderData }) => {
+    //               addClassName(renderData.blockAst, "not-content");
+    //             },
+    //           },
+    //         },
+    //       ];
+
+    //       return {
+    //         themeCssSelector: (theme, { styleVariants }) => {
+    //           // If one dark and one light theme are available, and the user has not disabled it,
+    //           // generate theme CSS selectors compatible with Starlight's dark mode switch
+    //           if (styleVariants.length >= 2) {
+    //             const baseTheme = styleVariants[0]?.theme;
+    //             const altTheme = styleVariants.find(
+    //               (v) => v.theme.type !== baseTheme?.type
+    //             )?.theme;
+    //             if (theme === baseTheme || theme === altTheme)
+    //               return `[data-theme='${theme.type}']`;
+    //           }
+    //           // Return the default selector
+    //           return `[data-theme='${theme.name}']`;
+    //         },
+    //         plugins,
+    //       };
+    //     },
+    //   },
+    // }),
     starlight({
       title: "Astro Digital Garden",
       social: {
@@ -100,6 +139,7 @@ export default defineConfig({
       ],
     ],
     rehypePlugins: [
+      // [rehypeMermaid, { strategy: "img-svg" }],
       rehypeHeadingIds,
       [rehypeAutolinkHeadings, { behavior: "append" }],
       [
